e235561d81cf9368b9d8ac14293616d822925827
ybellaho <yanis.bellahouel76@gmail.com>
2025-05-06 10:14:54 +0200

    component field

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CardList from './components/cardList/CardList.jsx';
+import Field from './components/field/Field.jsx';
 
 function App() {
 	return (
 		<div>
 			<h1>Card game</h1>
 			<CardList/>
+			<Field/>
 		</div>
 	)
 }
---
+++ src/components/field/Field.jsx
@@ -0,0 +1,44 @@
+import "./Field.css"
+
+function Field() {
+	return (
+		<div className="field">
+			<div className="fieldspell-box">
+			</div>
+			<div className="monster-and-spell-box">
+				<div className="monster-box">
+					<div className="monster-box-card">
+					</div>
+					<div className="monster-box-card">
+					</div>
+					<div className="monster-box-card">
+					</div>
+					<div className="monster-box-card">
+					</div>
+					<div className="monster-box-card">
+					</div>
+				</div>
+				<div className="spell-box">
+					<div className="spell-box-card">
+					</div>
+					<div className="spell-box-card">
+					</div>
+					<div className="spell-box-card">
+					</div>
+					<div className="spell-box-card">
+					</div>
+					<div className="spell-box-card">
+					</div>
+				</div>
+			</div>
+			<div className="graveyard-and-deck-box">
+				<div className="graveyard-box">
+				</div>
+				<div className="deck-box">
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Field;
